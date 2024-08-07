129489207321a698c96a1e55282c5f47d6f8e26b
jungbyungwook <korjby400@gmail.com>
2024-08-07 15:24:01 +0900

    feat: 상품 무한스크롤

--- src/apis/commerce.ts
+++ src/apis/commerce.ts
@@ -1,6 +1,7 @@
 import { api } from "./createApi";
+import { Product } from "@/data/interface/Product";
 
-export const getProducts = async (page: number, perPage: number) => {
+export const getProducts = async (page: number, perPage: number = 12) => {
     try {
         const res = await api.get(
             `/products?_page=${page}&_per_page=${perPage}`
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
 import { getProducts } from "@/apis/commerce";
 import ProductList from "@/components/ProductList";
 
 export default async function Home() {
-    const data = await getProducts(1, 10);
+    const initialData = await getProducts(1);
 
-    if (!data) {
+    if (!initialData) {
         return null;
     }
 
-    const renderProducts = () => {
-        return <ProductList items={data} />;
+    const renderProductList = () => {
+        return <ProductList initialData={initialData} />;
     };
 
-    return <main className="flex justify-center">{renderProducts()}</main>;
+    return <main className="flex justify-center">{renderProductList()}</main>;
 }
